848e4a8546b8a8f61236b9b4773ed784ac4d7de9
Morgan <Mharrisstoertz@gmail.com>
2023-04-20 17:03:35 -0400

    Created a scrollable list to display found songs. (Finally got source control working)

---
+++ Movies/Views/MovieDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailView.swift
+//  Movies
+//
+//  Created by Morgan Harris-Stoertz on 2023-04-20.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailView()
+    }
+}
---
+++ Movies/Views/SearchView.swift
@@ -0,0 +1,33 @@
+//
+//  SearchView.swift
+//  Movies
+//
+//  Created by Morgan Harris-Stoertz on 2023-04-20.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    //MARK: Stored properties
+    @State var foundMovies: [Movie] = []
+    
+    //MARK: Computed properties
+    var body: some View {
+        List(foundMovies){ currentMovie in
+            VStack{
+                Text(currentMovie.title)
+                    .bold()
+                Text(currentMovie.director)
+                    .italic()
+            }
+        }
+                
+                
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
