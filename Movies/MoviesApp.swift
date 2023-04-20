ccd252f9e118785cea3af257406ec4d344086e5a
Morgan <Mharrisstoertz@gmail.com>
2023-04-20 15:56:46 -0400

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Morgan Harris-Stoertz on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
