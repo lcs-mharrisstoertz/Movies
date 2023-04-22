3de90a0954e63f4151c074e67f6e6733bea7d24a
Morgan <Mharrisstoertz@gmail.com>
2023-04-22 10:51:58 -0400

    spent a couple hours trying to get a list to show up and couldn't get it to work so am now making static layout to get random movie to appear

--- Movies/Views/SearchView.swift
+++ Movies/Views/SearchView.swift
@@ -5,29 +5,72 @@
 //  Created by Morgan Harris-Stoertz on 2023-04-20.
 //
 
-import SwiftUI
+//import SwiftUI
+//
+//struct SearchView: View {
+//    //MARK: Stored properties
+//   @State var foundMovies: [Movie] = []
+//
+//    //MARK: Computed properties
+//    var body: some View {
+//        List(foundMovies, id: \.self){ movie in
+//            VStack(alignment: .leading){
+//                Text(movie.title)
+//                    .bold()
+//                Text(movie.director)
+//                    .italic()
+//            }
+//        }
+//        .task {
+//            foundMovies = await NetworkService.fetch()
+//        }
+//
+//    }
+//}
+//
+//struct SearchView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SearchView()
+//    }
+//}
 
-struct SearchView: View {
-    //MARK: Stored properties
-    @State var foundMovies: [Movie] = []
-    
-    //MARK: Computed properties
-    var body: some View {
-        List(foundMovies, id: \.self){ currentMovie in
-            VStack{
-                Text(currentMovie.title)
-                    .bold()
-                Text(currentMovie.director)
-                    .italic()
-            }
-        }
-                
-                
-    }
-}
 
-struct SearchView_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchView()
-    }
-}
+
+
+
+
+
+
+
+
+
+
+//import SwiftUI
+//
+//struct SearchView: View {
+//    //MARK: Stored properties
+//   @State var foundMovies: [Movie] = []
+//
+//    //MARK: Computed properties
+//    var body: some View {
+//
+//        List(foundMovies, id: \.self){ currentMovie in
+//            VStack{
+//                Text(currentMovie.title)
+//                    .bold()
+//                Text(currentMovie.director)
+//                    .italic()
+//            }
+//        }
+//        .task {
+//            foundMovies = await NetworkService.fetch()
+//        }
+//
+//    }
+//}
+//
+//struct SearchView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SearchView()
+//    }
+//}
