178f3c8f0cd0cedc207d97880fa49041690d7fa2
Morgan <Mharrisstoertz@gmail.com>
2023-04-22 13:29:49 -0400

    Adjusted the model so that a joke can be read from or written to the database. Again, can't fucking commit even though changes have been made - Started to add view to see the list of saved

---
+++ Movies/Views/FavouritesView.swift
@@ -0,0 +1,20 @@
+//
+//  FavouritesView.swift
+//  Movies
+//
+//  Created by Morgan Harris-Stoertz on 2023-04-22.
+//
+
+import SwiftUI
+
+struct FavouritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavouritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavouritesView()
+    }
+}
