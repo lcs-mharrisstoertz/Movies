b714c52cfb2b749c87cb9d6c06dbd8924348f4a6
Morgan <Mharrisstoertz@gmail.com>
2023-04-22 13:45:55 -0400

    added what should be a list that shows movie title, rating, and director. Again, not showing up cause why would it?

--- Movies/Views/FavouritesView.swift
+++ Movies/Views/FavouritesView.swift
@@ -4,17 +4,35 @@
 //
 //  Created by Morgan Harris-Stoertz on 2023-04-22.
 //
-
+import Blackbird
 import SwiftUI
 
 struct FavouritesView: View {
+    
+    //MARK: stored properties
+    @BlackbirdLiveModels({ db in
+        try await Movie.read(from: db)
+    }) var favouriteMovies
+    
+    //MARK: computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(favouriteMovies.results){currentMovie in
+            VStack(alignment: .leading){
+                Text(currentMovie.title)
+                    .bold()
+                HStack{
+                    Text(currentMovie.director)
+                    Text(currentMovie.rated)
+                }
+               
+            }
+        }
     }
 }
 
 struct FavouritesView_Previews: PreviewProvider {
     static var previews: some View {
         FavouritesView()
+            .environment(\.blackbirdDatabase, AppDatabase.instance)
     }
 }
